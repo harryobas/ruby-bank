2017dd6f23738858d0e31e83e544af8e55b70ba6
harry obaseki <harryobas@gmail.com>
2019-08-28 00:15:01 +0200

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < ActionController::API
+  before_action :validate_token
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find(auth["user"])
+  end
+
+  def validate_token
+    if !logged_in
+      render json: {error: "unauthorized"}, status: 401
+    end
+  end
+
+  private
+
+  def token
+    request.authorization.scan(/Bearer (.*)$/).flatten.last
+  end
+
+  def auth
+    Auth.decode(token)
+  end
+end
